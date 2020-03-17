a4b80b286a48bb80f8a378885a12e81fe7c1367f
Brad M <brmenchl@gmail.com>
2020-03-16 23:18:42 -0400

    downgrade to yarn 1.x

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ["./node_modules/poetic/config/eslint/eslint-config.js"],
+  extends: ['./node_modules/poetic/config/eslint/eslint-config.js'],
   // Add custom rules here
-  rules: {}
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'func-names': ['error', 'always', { generators: 'never' }],
+    'import/prefer-default-export': 'off',
+    'react/prop-types': 'off',
+  },
 };
