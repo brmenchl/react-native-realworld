d9d8a53e5fb15f922a18ec164294700a789efe4e
Brad M <brmenchl@gmail.com>
2020-03-21 17:09:27 -0400

    add native base for ui

--- App.tsx
+++ App.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
+import * as Font from 'expo-font';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Ionicons } from '@expo/vector-icons';
 import { store } from './src/store';
 import RootNavigator from './src/navigation/RootNavigator';
 
-const App = () => (
-  <Provider store={store}>
-    <RootNavigator />
-  </Provider>
-);
+const App = () => {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    }).then(() => {
+      setIsReady(true);
+    });
+  }, []);
+  return isReady ? (
+    <Provider store={store}>
+      <RootNavigator />
+    </Provider>
+  ) : null;
+};
 
 export default App;
